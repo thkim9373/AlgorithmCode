7e4f4755af762e891b61d402973d89e5f08122fe
TaeHoon <thkim9373@gmail.com>
2020-01-29 23:28:10 +0900

    1. No.1018 solved.

---
+++ src/baekjoon/Num1018.java
@@ -0,0 +1,65 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Num1018 {
+    private static char[][] board;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        int row = Integer.parseInt(tokenizer.nextToken());
+        int column = Integer.parseInt(tokenizer.nextToken());
+
+        board = new char[row][column];
+
+        for (int i = 0; i < row; i++) {
+            String inputRow = reader.readLine();
+            for (int j = 0; j < column; j++) {
+                board[i][j] = inputRow.charAt(j);
+            }
+        }
+
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i <= row - 8; i++) {
+            for (int j = 0; j <= column - 8; j++) {
+                result = Math.min(result, getPaintCount(i, j, 'W'));
+                result = Math.min(result, getPaintCount(i, j, 'B'));
+            }
+        }
+
+        writer.write(result + " ");
+
+        reader.close();
+        writer.close();
+    }
+
+    private static int getPaintCount(int startRow, int startColumn, char startColor) {
+        int result = 0;
+        char color = startColor;
+        for (int a = startRow; a < startRow + 8; a++) {
+
+            for (int b = startColumn; b < startColumn + 8; b++) {
+                if (board[a][b] != color) {
+                    result++;
+                }
+                if (color == 'W') {
+                    color = 'B';
+                } else {
+                    color = 'W';
+                }
+            }
+
+            if (color == 'W') {
+                color = 'B';
+            } else {
+                color = 'W';
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/baekjoon/Num1436.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Num1436 {
+    public void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int numOfResult = Integer.parseInt(reader.readLine());
+
+
+
+        reader.close();
+        writer.close();
+    }
+}
+
