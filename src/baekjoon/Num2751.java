ed183514bdf31783d1e9c5b2795a9cbfcd4f33d3
thkim9373 <thkim9373@gmail.com>
2020-02-03 18:32:42 +0900

    1. Num 2750 solve. 2. Num 2751 added.

---
+++ src/baekjoon/Num2751.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.*;
+
+// 수 정렬하기 2
+// https://www.acmicpc.net/problem/2751
+public class Num2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arrLength = Integer.parseInt(reader.readLine());
+        int[] arr = new int[arrLength];
+
+
+
+        reader.close();
+        writer.close();
+    }
+
+    private void shellSort(int[] arr, int arrLength) {
+
+    }
+}
