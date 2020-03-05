5c59e0d1aaf0c119f2216f8079cc28800a648a73
TaeHoon <thkim9373@gmail.com>
2020-03-05 23:44:51 +0900

    Save commit.

--- src/baekjoon/backtracking/Num2580.java
+++ src/baekjoon/backtracking/Num2580.java
@@ -1,69 +1,71 @@
 package baekjoon.backtracking;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.Stack;
 
 //  스도쿠
 //  https://www.acmicpc.net/problem/2580
 public class Num2580 {
     private static int[][] board = new int[9][9];
-    private static int[][] answer = new int[9][9];
+    private static Stack<Point> pointStack = new Stack<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         for (int i = 0; i < 9; i++) {
             String[] strings = reader.readLine().split(" ");
             for (int j = 0; j < 9; j++) {
-                board[i][j] = Integer.parseInt(strings[j]);
-            }
-        }
-
-        getAnswer(0, 0);
-
-        StringBuilder builder = new StringBuilder();
-        for (int[] ints : board) {
-            for (int i : ints) {
-                builder.append(i).append(" ");
-            }
-            builder.append("\n");
-        }
-
-        writer.write(builder.toString());
-
-        reader.close();
-        writer.close();
-    }
-
-    private static void getAnswer(int x1, int y1) {
-        if (isComplete()) {
-            for (int y = 0; y < 9; y++) {
-                System.arraycopy(board[y], 0, answer[y], 0, 9);
-            }
-        }
-        for (int y = y1; y < 9; y++) {
-            for (int x = x1; x < 9; x++) {
-                if (board[y][x] == 0) {
-                    boolean[] possibleNum = getPossibleNum(x, y);
-                    for (int i = 0; i < possibleNum.length; i++) {
-                        if (possibleNum[i]) {
-                            board[y][x] = i + 1;
-                            getAnswer(x, y);
-                        }
-                    }
+                int a = Integer.parseInt(strings[j]);
+                board[i][j] = a;
+                if (a == 0) {
+                    pointStack.add(new Point(j, i));
                 }
             }
         }
+
+        getAnswer();
+
+        reader.close();
     }
 
-    private static boolean[] getPossibleNum(int x, int y) {
-        boolean[] result = new boolean[9];
-        Arrays.fill(result, true);
+    private static void getAnswer() {
+        if (pointStack.empty()) {
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            StringBuilder builder = new StringBuilder();
+            for (int[] ints : board) {
+                for (int i : ints) {
+                    builder.append(i).append(" ");
+                }
+                builder.append("\n");
+            }
+            try {
+                writer.write(builder.toString());
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.exit(0);
+        }
+        Point point = pointStack.pop();
+        for (int i = 1; i <= 9; i++) {
+            if (isPossible(point, i)) {
+                board[point.y][point.x] = i;
+                getAnswer();
+                board[point.y][point.x] = 0;
+            }
+        }
+        pointStack.push(point);
+    }
+
+    private static boolean isPossible(Point point, int num) {
+        int x = point.x;
+        int y = point.y;
 
         for (int i = 0; i < 9; i++) {
-            if (board[y][i] != 0) result[board[y][i] - 1] = false;
-            if (board[i][x] != 0) result[board[i][x] - 1] = false;
+            if (board[y][i] == num) return false;
+            if (board[i][x] == num) return false;
         }
 
         int xMin, xMax;
@@ -77,19 +79,20 @@ public class Num2580 {
 
         for (int i = yMin; i < yMax; i++) {
             for (int j = xMin; j < xMax; j++) {
-                if (board[i][j] != 0) result[board[i][j] - 1] = false;
+                if (board[i][j] == num) return false;
             }
         }
 
-        return result;
+        return true;
     }
 
-    private static boolean isComplete() {
-        for (int[] ints : board) {
-            for (int i : ints) {
-                if (i == 0) return false;
-            }
+    private static class Point {
+        int x;
+        int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
-        return true;
     }
 }
