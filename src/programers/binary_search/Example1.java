0c2e4f03a8de50de06a80b5e5333d39685b1cb1c
TaeHoon <thkim9373@gmail.com>
2020-03-27 23:52:44 +0900

    Save commit.

--- src/programers/binary_search/Example1.java
+++ src/programers/binary_search/Example1.java
@@ -1,39 +1,40 @@
 package programers.binary_search;
 
-import javax.swing.*;
 import java.util.Arrays;
-import java.util.Collections;
 
+//  예산
+//  https://programmers.co.kr/learn/courses/30/lessons/43237
 public class Example1 {
     public static void main(String[] args) {
 //        new int[]{120, 110, 140, 150}, 485)
-        System.out.println(solution(new int[]{100, 100, 100, 100}, 400));
+//        new int[]{100, 100, 100, 100}, 400)
+        System.out.println(solution(new int[]{120, 110, 140, 150}, 485));
     }
 
     public static int solution(int[] budgets, int M) {
         int answer = 0;
 
         Arrays.sort(budgets);
-        int maxResult = budgets[budgets.length - 1];
-        int minResult = 0;
-        int temp = maxResult / 2;
-
-        while (maxResult > minResult) {
+        int max = budgets[budgets.length - 1];
+        int min = 0;
+        int temp;
+        while (max >= min) {
+            temp = (max + min) / 2;
             int possibleBudget = getPossibleBudget(budgets, temp);
             if (possibleBudget >= M) {
-                maxResult = temp;
-                temp = (minResult + temp -1) / 2;
+                max = temp - 1;
             } else {
-                minResult = temp;
-                temp = (maxResult + temp) / 2;
+                min = temp + 1;
+            }
+            if (possibleBudget <= M) {
+                answer = temp;
             }
         }
 
-        answer = temp;
         return answer;
     }
 
-    public static int getPossibleBudget(int[] budgets, int boundary) {
+    private static int getPossibleBudget(int[] budgets, int boundary) {
         int result = 0;
         for (int budget : budgets) {
             result += Math.min(boundary, budget);
---
+++ src/programers/binary_search/Example2.java
@@ -0,0 +1,15 @@
+package programers.binary_search;
+
+import java.io.IOException;
+
+public class Example2 {
+    public static void main(String[] args) {
+//        6, new int[] {7, 10}
+        System.out.println(solution(6, new int[] {7, 10}));
+    }
+
+    public static long solution(int n, int[] times) {
+        long answer = 0;
+        return answer;
+    }
+}
